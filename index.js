458ae481f428294b2fcff940266a32d20b4e0736
xiaoshao <494018648@qq.com>
2017-04-19 17:42:18 +0800

    fix log with log4js

--- index.js
+++ index.js
@@ -2,32 +2,20 @@ let express = require('express');
 let app = express()
 let md5 = require('md5')
 let compression = require('compression')
-let logger = require('morgan')
+let path = require('path')
+// let logger = require('morgan')
 let cookieParser = require('cookie-parser')
 let bodyParser = require('body-parser')
 let session = require('express-session')
 let port = 3000
 const errorMessge =  require('./config/statusCode')
+let logger = require('./logger/config')('access')
 require('./model/rootAdmin')
 require('./model/product')
 require('./model/user')
 let {adminRouter, userRouter, productRouter} = require('./router/router')
-let fs = require('fs')
-let path = require('path')
-let rfs = require('rotating-file-stream')
-
-var logDirectory = path.join(__dirname, 'logs')
-// ensure log directory exists
-fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
-
-// create a rotating write stream
-var accessLogStream = rfs('access.log', {
-  interval: '2d', // rotate daily
-  path: logDirectory
-})
 
 app.use(compression())
-app.use(logger(':remote-addr - :remote-user [:date[clf]] ":method HTTP/:http-version :url :req[body] " :status :res[content-length] -- :response-time[digits] ms', {stream: accessLogStream}))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
@@ -44,6 +32,7 @@ app.use(session({
 
 app.all('*', (req, res, next) => {
   debugger
+  logger.info(`${req.method} ${req.protocol} ${req.ip} ${req.url} ${JSON.stringify(req.body)} ${req.headers['user-agent']} `)
   if (!req.url.match(/\/(login|regist)$/) && !req.session.userId) {
     res.json({
       statusCode: 2000403,
