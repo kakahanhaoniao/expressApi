4fb59ff7750c45d854dbe4d52827067b3fd9dfd0
xiao shao <494018648@qq.com>
2017-04-19 23:13:38 +0800

    fix add response time

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ require('./model/rootAdmin')
 require('./model/product')
 require('./model/user')
 let {adminRouter, userRouter, productRouter} = require('./router/router')
+var responseTime = require('response-time')
+var StatsD = require('node-statsd')
+var stats = new StatsD()
+
+stats.socket.on('error', function (error) {
+  console.error(error.stack)
+})
+
 
 app.use(compression())
 app.use(bodyParser.json())
@@ -29,10 +37,13 @@ app.use(session({
   cookie: { secure: false, expires:1000*60*60*24 }
 }))
 
+app.use(responseTime(function (req, res, time) {
+  logger.info(`${req.method} ${req.protocol} ${req.ip} ${req.originalUrl} ${JSON.stringify(req.body)} ${req.headers['user-agent']}  ${res.statusCode}   ${time}ms`)
+}))
 
+// TODo
 app.all('*', (req, res, next) => {
   debugger
-  logger.info(`${req.method} ${req.protocol} ${req.ip} ${req.url} ${JSON.stringify(req.body)} ${req.headers['user-agent']} `)
   if (!req.url.match(/\/(login|regist)$/) && !req.session.userId) {
     res.json({
       statusCode: 2000403,
@@ -44,8 +55,21 @@ app.all('*', (req, res, next) => {
   console.log('拦截器一枚')
 })
 
+
 app.use('/api',adminRouter)
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    debugger
+    var err = new Error('Not Found')
+    err.status = 404
+    next(err)
+})
+
+app.use(function(err, req, res) {
+    res.status(err.status || 500)
+    res.send(err.message)
+})
 
 app.listen(port, function () {
   console.log('Example app listening on port 3000!');
