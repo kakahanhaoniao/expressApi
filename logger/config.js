6ca40359bb7647ff9a6f19df4d2f866424d17678
xiao shao <494018648@qq.com>
2017-04-26 22:18:27 +0800

    fix koa log4js creat logfile

--- logger/config.js
+++ logger/config.js
@@ -1,22 +1,26 @@
 const koa = require('koa');
-const log4js = require('log4js');
+const log4js = require('koa-log4');
 const app = new koa();
 
 log4js.configure({
-    appenders: [
-        { type: 'console' },
-        {
-            type: 'DateFile',
-            filename: 'logs/access.log',
-            pattern: '-yyyy-MM-dd.log',
-            alwaysIncludePattern: true,
-            category: 'access'
-         }
-    ],
-    replaceConsole: true
+  appenders: [
+    {
+      type: 'console'
+    },
+    {
+      type: 'DateFile',
+      filename: 'logs/access.log',
+      pattern: '-yyyy-MM-dd.log',
+      alwaysIncludePattern: true,
+      category: 'access'
+    }
+  ],
+  replaceConsole: true
 });
 
-app.use(log4js.connectLogger(log4js.getLogger('debug'), {format:':method :url'}));
+app.use(log4js.koaLogger(log4js.getLogger('debug'), {
+  format: ':method :url'
+}));
 
 module.exports = (name) => {
   var logger = log4js.getLogger(name);
