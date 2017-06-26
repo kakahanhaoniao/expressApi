d37360a91de619308fe648fb9c5954889e762ee5
xiao shao <494018648@qq.com>
2017-06-26 20:20:56 +0800

    test

--- logger/config.js
+++ logger/config.js
@@ -3,27 +3,27 @@ const log4js = require('koa-log4');
 const app = new koa();
 
 log4js.configure({
-  appenders: [
-    {
-      type: 'console'
-    },
-    {
-      type: 'DateFile',
-      filename: 'logs/access.log',
-      pattern: '-yyyy-MM-dd.log',
-      alwaysIncludePattern: true,
-      category: 'access'
-    }
-  ],
-  replaceConsole: true
+    appenders: [
+        {
+            type: 'console'
+        },
+        {
+            type: 'DateFile',
+            filename: 'logs/access.log',
+            pattern: '-yyyy-MM-dd.log',
+            alwaysIncludePattern: true,
+            category: 'access'
+        }
+    ],
+    replaceConsole: true
 });
 
 app.use(log4js.koaLogger(log4js.getLogger('debug'), {
-  format: ':method :url'
+    format: ':method :url'
 }));
 
 module.exports = (name) => {
-  var logger = log4js.getLogger(name);
-  logger.setLevel('INFO');
-  return logger;
+    var logger = log4js.getLogger(name);
+    logger.setLevel('INFO');
+    return logger;
 };
